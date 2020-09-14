a15c6946335db8c69529972f9da20ab1351c15f6
Jonathan Mason <jonathanandmargaret.mason@gmail.com>
2020-09-14 14:59:04 +0100

    Added improved moon image for when there is no background moon.

--- app/src/main/java/jonathan/mason/moondial/MainActivity.kt
+++ app/src/main/java/jonathan/mason/moondial/MainActivity.kt
@@ -130,8 +130,19 @@ class MainActivity(var currentPhase: Phases = Phases.calculateCurrentPhase()) : 
      * Update image displaying phase of moon and its description.
      */
     private fun updatePhase() {
-        //imageViewForeground.setImageResource(if(currentPhase != Phases.NewMoon) currentPhase.drawable else 0)
-        imageViewForeground.setImageResource(currentPhase.drawable)
+        if(currentPhase == Phases.NewMoon) {
+            val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
+            val displayBackgroundMoon = sharedPrefs.getBoolean(getString(R.string.background_moon_key), resources.getBoolean(R.bool.background_moon_default))
+            if(displayBackgroundMoon)
+                imageViewForeground.setImageResource(0)
+            else
+                imageViewForeground.setImageResource(currentPhase.drawable)
+        }
+        else
+        {
+            imageViewForeground.setImageResource(currentPhase.drawable)
+        }
+
         textViewPhaseDescription.text = this.getText(currentPhase.stringName)
     }
 }
